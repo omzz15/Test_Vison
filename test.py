78106594a34f07a563ed94877b70cbbf0bc8f469
omzz15 <drompatel5@gmail.com>
2023-01-14 16:55:55 -0500

    works?

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
 from cscore import CameraServer
 import cv2
 import numpy as np
+import pupil_apriltags
+
+#make apriltag detector
+detector = pupil_apriltags.Detector(families='tag36h11')
+
 
 width = 480
 height = 320
@@ -28,15 +33,19 @@ while True:
 
    if time == 0:
       # Send the output the error.
-      outputStream.notifyError(cvSink.getError());
+      outputStream.notifyError(cvSink.getError())
       # skip the rest of the current iteration
       continue
 
-   #
-   # Insert your image processing logic here!
-   #
+   img_2 = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-   
+   result : list[pupil_apriltags.Detection] = detector.detect(img = img_2)
+
+   for r in result:
+      img = cv2.line(img, tuple(map(int, r.corners[0])), tuple(map(int, r.corners[1])), (0,0,255), 5)
+      img = cv2.line(img, tuple(map(int, r.corners[1])), tuple(map(int, r.corners[2])), (0,0,255), 5)
+      img = cv2.line(img, tuple(map(int, r.corners[2])), tuple(map(int, r.corners[3])), (0,0,255), 5)
+      img = cv2.line(img, tuple(map(int, r.corners[3])), tuple(map(int, r.corners[0])), (0,0,255), 5)
 
    # (optional) send some image back to the dashboard
    outputStream.putFrame(img)
--- test2.py
+++ test2.py
@@ -1,1 +1,30 @@
-print("hello world")
+import cv2
+import numpy as np
+import pupil_apriltags
+
+#make apriltag detector
+detector = pupil_apriltags.Detector(families='tag36h11')
+
+vid = cv2.VideoCapture(0)
+
+while(True):
+    ret, img = vid.read()
+    
+    img_2 = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    result : list[pupil_apriltags.Detection] = detector.detect(img = img_2, estimate_tag_pose = True)
+
+    for r in result:
+        print(r.pose_R)
+        img = cv2.line(img, tuple(map(int, r.corners[0])), tuple(map(int, r.corners[1])), (0,0,255), 5)
+        img = cv2.line(img, tuple(map(int, r.corners[1])), tuple(map(int, r.corners[2])), (0,0,255), 5)
+        img = cv2.line(img, tuple(map(int, r.corners[2])), tuple(map(int, r.corners[3])), (0,0,255), 5)
+        img = cv2.line(img, tuple(map(int, r.corners[3])), tuple(map(int, r.corners[0])), (0,0,255), 5)
+
+    cv2.imshow("test", img)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+vid.release()
+cv2.destroyAllWindows()
